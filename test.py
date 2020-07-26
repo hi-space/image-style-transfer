944d64cf5c4d88696c0fd63394421175654a629c
hispace <hispace.j@gmail.com>
2020-07-26 19:56:56 +0900

    test codes

---
+++ test.py
@@ -0,0 +1,39 @@
+import re
+import os
+import torch
+import torchvision.transforms as transforms
+
+from models import TransformerNet
+from utils import load_image, save_image
+from test_config import *
+
+
+if __name__ == "__main__":    
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print('device: ', device)
+
+    content_image = load_image(test_image, scale=2) #FIXME
+    content_transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Lambda(lambda x: x.mul(255))
+    ])
+    content_image = content_transform(content_image)
+    content_image = content_image.unsqueeze(0).to(device)
+
+    with torch.no_grad():
+        style_model = TransformerNet()
+        
+        ckpt_model_path = os.path.join(checkpoint_dir, checkpoint_file)
+        checkpoint = torch.load(ckpt_model_path, map_location=device)
+        
+        # remove saved deprecated running_* keys in InstanceNorm from the checkpoint
+        for k in list(checkpoint.keys()):
+            if re.search(r'in\d+\.running_(mean|var)$', k):
+                del checkpoint[k]
+        
+        style_model.load_state_dict(checkpoint['model_state_dict'])
+        style_model.to(device)
+        
+        output = style_model(content_image).cpu()
+        
+    save_image(output_image, output[0]) #FIXME
--- test_config.py
+++ test_config.py
@@ -1,8 +1,9 @@
 
-checkpoint_dir = "data_sn/"
-checkpoint_file = "ckpt_epoch_63_batch_id_500.pth"
+checkpoint_dir = 'checkpoint/'
+checkpoint_file = 'ckpt_epoch_63_batch_id_500.pth'
 
-source_file = "midnight_in_paris.mp4"
-output_file = "result.mp4"
+source_file = 'midnight_in_paris.mp4'
+output_file = 'result.mp4'
 
-debug_folder = "result/"
+test_image = 'sample_image.png'
+output_image = 'sample_result.png'
